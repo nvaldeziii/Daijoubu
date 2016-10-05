be9bb9ae36de2b3a8115709e3135258ec564c898
Noli Valdez II <nolivaldeziii@gmail.com>
2016-10-05 10:48:45 +0800

    wireframe initiated

---
+++ Daijoubu/Daijoubu/AppTemplates/HomepageListView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Daijoubu.AppTemplates
+{
+    public partial class HomepageListView : ContentPage
+    {
+        public HomepageListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
