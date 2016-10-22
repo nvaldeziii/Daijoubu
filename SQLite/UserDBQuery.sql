1fe409d47a6836a62a09b156d9b6ae52fde12b05
Noli Valdez III <nolivaldeziii@gmail.com>
2016-10-22 20:33:03 +0800

    updated sql

--- SQLite/UserDBQuery.sql
+++ SQLite/UserDBQuery.sql
@@ -11,14 +11,36 @@ create table tbl_us_cardknN5Dt(
         LastView varchar(254)
 );
 insert into tbl_us_cardknN5Dt values (null,0,0,0);
-create table tbl_user_card_vocab_N5data(
+
+create table tbl_us_cardktknN5Dt(
+		Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        CorrectCount int,
+        MistakeCount int,
+        LastView varchar(254)
+);
+insert into tbl_us_cardktknN5Dt values (null,0,0,0);
+
+create table tbl_us_cardvbN5dt(
 		Id INTEGER PRIMARY KEY AUTOINCREMENT,
         CorrectCount int,
         MistakeCount int,
         LastView varchar(254)
 );
 
-create table tbl_user_card_kana_N4Data(
+create table tbl_us_cardvbN4dt(
+		Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        CorrectCount int,
+        MistakeCount int,
+        LastView varchar(254)
+);
+
+create table tbl_us_cardknN4Dt(
+		Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        CorrectCount int,
+        MistakeCount int,
+        LastView varchar(254)
+		
+);create table tbl_us_cardktknN4Dt(
 		Id INTEGER PRIMARY KEY AUTOINCREMENT,
         CorrectCount int,
         MistakeCount int,
