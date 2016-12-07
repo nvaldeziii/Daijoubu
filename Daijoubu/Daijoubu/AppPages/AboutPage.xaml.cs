991cd2386ba647b5a52d99f79fbe27dd53b9bbd7
Noli Valdez III <nolivaldeziii@gmail.com>
2016-12-07 17:42:21 +0800

    bumped to version 1.2.0

--- Daijoubu/Daijoubu/AppPages/AboutPage.xaml.cs
+++ Daijoubu/Daijoubu/AppPages/AboutPage.xaml.cs
@@ -16,7 +16,7 @@ namespace Daijoubu.AppPages
         {
             InitializeComponent();
 
-            label_version_info.Text = string.Format("Version: {0}", "1.1.0");
+            label_version_info.Text = string.Format("Version: {0}", "1.2.0");
         }
 
     }
